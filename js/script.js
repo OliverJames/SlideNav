6492cde4304ff44f23c24aea73bf086be9d4cbad
Oliver <olivermahoney@hotmail.com>
2013-09-08 20:50:03 +0100

    Update script.js
    Clean up and change to better method of achieving the Navigation Movement.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,9 @@
 // JavaScript Document By Oliver Mahoney
 
-$(function() {
-	$( "#display, .mainMenuHidden" ).click(function(){
+$(document).ready(function() {
+	$( "#displayButton" ).click(function(){
 	  $( ".mainMenu" ).switchClass( "mainMenu", "mainMenuHidden", 500 );
 	  $( ".mainMenuHidden" ).switchClass( "mainMenuHidden", "mainMenu", 500 );
 	  return false;
-	});
-	$("#main").mouseenter(function(e){
-		$( ".main" ).switchClass( "main", "mainShadow", 500 );
-		return false;
-	});
-	$("#main").mouseleave(function(e){
-		$( ".mainShadow" ).switchClass( "mainShadow", "main", 500 );
-		return false;
 	});
 });
